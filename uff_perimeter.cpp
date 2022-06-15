f02c76bd0d0813bd777b2a643b3fe4102d7e760d
Vincent LE GARREC <vincent.legarrec@moduleus.com>
2022-06-15 17:38:30 +0200

    Handle HDF5 with FetchContent

--- uff_perimeter.cpp
+++ uff_perimeter.cpp
@@ -13,7 +13,7 @@ void Perimeter::printSelf(std::ostream& os, std::string indent) const
 {
     superclass::printSelf(os, indent);
     os << indent << "Points: (";
-    for (const auto pt : this->m_points)
+    for (const auto & pt : this->m_points)
     {
         os << "(" << pt.first << ", " << pt.second << "), ";
     }
