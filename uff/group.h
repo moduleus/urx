6f472b0a622b8a6f99d75e1d1c237a0b3e3bfb1d
Thibault GEFFROY <thibault.geffroy@moduleus.com>
2023-09-06 13:34:11 +0200

    Move SamplingType from ReceiveSetup to Group

--- uff/group.h
+++ uff/group.h
@@ -8,11 +8,16 @@
 namespace uff {
 
 struct Group : public IGroup {
+  enum class SamplingType { DIRECT_RF = 0, IQ = 1 };
+
   bool operator==(const Group& other) const {
-    return (IGroup::operator==(other) && repetition_rate == other.repetition_rate &&
-            sequence == other.sequence);
+    return (IGroup::operator==(other) && sampling_type == other.sampling_type &&
+            repetition_rate == other.repetition_rate && sequence == other.sequence);
   }
   inline bool operator!=(const Group& other) const { return !(*this == other); }
+
+  // Type of sampling:
+  SamplingType sampling_type = SamplingType::DIRECT_RF;
 
   Sequence sequence;
 
