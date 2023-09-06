0d029b839e66c528faad3a56819ffd9d65cc05a0
Thibault GEFFROY <thibault.geffroy@moduleus.com>
2023-09-06 11:44:01 +0200

    Move UpdateElements function out of probe classes

--- uff/linear_array.h
+++ uff/linear_array.h
@@ -14,35 +14,11 @@ namespace uff {
  * @brief The UFF LinearArray class describes a linear array (1D)
  */
 struct LinearArray : public Probe {
-  LinearArray() = delete;
-  explicit LinearArray(uint32_t number_elements, double pitch,
-                       const std::optional<Vector2D<double>>& element_size = std::nullopt)
-      : nb_elements(number_elements), pitch(pitch), element_size(element_size) {
-    updateElements();
-  }
-  LinearArray(const LinearArray&) = default;
-  LinearArray(LinearArray&&) = default;
-  ~LinearArray() = default;
-
-  // Operators
-  LinearArray& operator=(const LinearArray& other) noexcept = default;
-  LinearArray& operator=(LinearArray&& other) noexcept = default;
   inline bool operator==(const LinearArray& other) const {
     return (Probe::operator==(other) && nb_elements == other.nb_elements && pitch == other.pitch &&
             element_size == other.element_size);
   }
   inline bool operator!=(const LinearArray& other) const { return !(*this == other); }
-
-  // Update elements position
-  void updateElements() {
-    elements.resize(nb_elements);
-    for (uint32_t i = 0; i < nb_elements; ++i) {
-      // element position
-      double xmin = -pitch * static_cast<float>(nb_elements - 1) / 2.f;
-      elements[i] =
-          Element({Vector3D<double>{xmin + i * pitch, 0., 0.}, Vector3D<double>{0., 0., 0.}});
-    }
-  }
 
   // Number of elements in the array
   uint32_t nb_elements = 0;
--- uff/rca_array.h
+++ uff/rca_array.h
@@ -16,45 +16,11 @@ namespace uff {
  * @brief The UFF RCA Array class describes a RCA (Row Column Address) probe
  */
 struct RcaArray : public Probe {
-  RcaArray() = delete;
-  explicit RcaArray(Vector2D<uint32_t> nb_elements, const Vector2D<double>& pitch,
-                    const std::optional<Vector2D<double>>& element_width = std::nullopt,
-                    const std::optional<Vector2D<double>>& element_height = std::nullopt)
-      : nb_elements(nb_elements),
-        pitch(pitch),
-        element_width(element_width),
-        element_height(element_height) {
-    updateElements();
-  }
-  RcaArray(const RcaArray&) = default;
-  RcaArray(RcaArray&&) = default;
-  ~RcaArray() = default;
-
-  // Operators
-  RcaArray& operator=(const RcaArray& other) noexcept = default;
-  RcaArray& operator=(RcaArray&& other) noexcept = default;
   inline bool operator==(const RcaArray& other) const {
     return (Probe::operator==(other) && nb_elements == other.nb_elements && pitch == other.pitch &&
             element_width == other.element_width && element_height == other.element_height);
   }
   inline bool operator!=(const RcaArray& other) const { return !(*this == other); }
-
-  // Update elements position
-  void updateElements() {
-    elements.resize(static_cast<size_t>(nb_elements.x) + nb_elements.y);
-
-    const double xmin = -pitch.x * (nb_elements.x - 1.) / 2.;
-    for (uint32_t i = 0; i < nb_elements.x; i++) {
-      elements[i] =
-          Element({Vector3D<double>{xmin + i * pitch.x, 0., 0.}, Vector3D<double>{0., 0., 0.}});
-    }
-
-    const double ymin = -pitch.y * (nb_elements.y - 1.) / 2.;
-    for (uint32_t i = nb_elements.y; i < elements.size(); i++) {
-      elements[i] = Element({Vector3D<double>{0., ymin + (i - nb_elements.y) * pitch.y, 0.},
-                             Vector3D<double>{0., 0., 0.}});
-    }
-  }
 
   // Number of elements in the axis x and y
   Vector2D<uint32_t> nb_elements{0u, 0u};
