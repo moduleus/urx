5d1829d1b09993cea2bf55d9901074311e7fa2fe
Thibault GEFFROY <thibault.geffroy@moduleus.com>
2023-09-13 17:09:27 +0200

    Remove channel_id from ChannelData

--- uff/channel_data.h
+++ uff/channel_data.h
@@ -9,14 +9,12 @@ template <typename T>
 struct ChannelData {
   inline bool operator==(const ChannelData<T>& other) const {
     return (sample_data.begin() == other.sample_data.begin()) &&
-           (sample_data.end() == other.sample_data.end()) && (channel_id == other.channel_id);
+           (sample_data.end() == other.sample_data.end());
   }
   inline bool operator!=(const ChannelData<T>& other) const { return !(*this == other); }
 
   // Data organized as _sample_data[ReceiveSetup::_number_samples]
   std::span<T> sample_data;
-
-  std::optional<uint16_t> channel_id;
 };
 
 }  // namespace uff
