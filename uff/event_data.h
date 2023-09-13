80ea9e53556708f2d6841d083daff20e95273823
Thibault GEFFROY <thibault.geffroy@moduleus.com>
2023-09-13 17:14:13 +0200

    Remove event from EventData

--- uff/event_data.h
+++ uff/event_data.h
@@ -11,9 +11,7 @@ namespace uff {
 template <typename T>
 struct EventData {
   inline bool operator==(const EventData<T>& other) const {
-    return (event.expired() == other.event.expired()) &&
-           (event.expired() || *(event.lock()) == *(other.event.lock())) &&
-           (channel_data == other.channel_data) && (event_timestamp == other.event_timestamp);
+    return (channel_data == other.channel_data) && (event_timestamp == other.event_timestamp);
   }
   inline bool operator!=(const EventData<T>& other) const { return !(*this == other); }
 
@@ -21,8 +19,6 @@ struct EventData {
   std::vector<ChannelData<T>> channel_data;
 
   double event_timestamp = std::numeric_limits<double>::max();
-
-  std::weak_ptr<TimedEvent> event;
 };
 
 }  // namespace uff
