589ba4e74d56165fc6ebe4ebf4305d67e361fa0e
Thibault GEFFROY <thibault.geffroy@moduleus.com>
2023-11-14 11:14:12 +0100

    Include pybind11 operators header to avoid wrong comparaison of uff types using == or =!

--- python/bindings.cpp
+++ python/bindings.cpp
@@ -1,6 +1,7 @@
 #include <pybind11/complex.h>
 #include <pybind11/functional.h>
 #include <pybind11/numpy.h>
+#include <pybind11/operators.h>
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <pybind11/stl_bind.h>
