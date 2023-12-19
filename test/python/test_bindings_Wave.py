b4aa281a139bcee5e7611284bb6939de4243c002
Thibault GEFFROY <thibault.geffroy@moduleus.com>
2023-12-19 17:06:53 +0100

    Wave

--- test/python/test_bindings_Wave.py
+++ test/python/test_bindings_Wave.py
@@ -38,42 +38,76 @@ class TestBindingsWave(unittest.TestCase):
         w_2 = urx.Wave(w)
         self.assertEqual(w, w_2)
 
-        w.time_zero.value = 132
+        self.assertEqual(w.time_zero, 0)
+        time_zero_ref = w.time_zero
+        time_zero_ref.value = 123
+        self.assertEqual(w.time_zero, time_zero_ref)
         self.assertNotEqual(w, w_2)
-        w_2 = urx.Wave(w)
-
-        w.time_zero_reference_point = urx.Vector3D(1, 2, 3)
-        self.assertNotEqual(w, w_2)
-        w_2 = urx.Wave(w)
-
-        w.channel_mapping = [[10, 11], [20, 21, 23]]
-        self.assertNotEqual(w, w_2)
-        w_2 = urx.Wave(w)
-
-        w.channel_excitations = [ex_3, ex_4]
-        self.assertNotEqual(w, w_2)
-        w_2 = urx.Wave(w)
-
-        w.channel_excitations[0] = ex_2
+        w.time_zero = urx.DoubleNan(132)
+        self.assertEqual(w.time_zero, time_zero_ref)
+        self.assertEqual(w.time_zero, 132)
+        w.time_zero = urx.DoubleNan(0)
         self.assertEqual(w, w_2)
-        w_2 = urx.Wave(w)
 
-        w.channel_delays = [7.89, 9.87]
+        self.assertEqual(w.time_zero_reference_point, urx.Vector3D())
+        time_zero_reference_point_ref = w.time_zero_reference_point
+        time_zero_reference_point_ref.x = 123
+        self.assertEqual(w.time_zero_reference_point,
+                         time_zero_reference_point_ref)
+        self.assertNotEqual(w, w_2)
+        w.time_zero_reference_point = urx.Vector3D()
+        self.assertEqual(w.time_zero_reference_point,
+                         time_zero_reference_point_ref)
+        self.assertEqual(w.time_zero_reference_point, urx.Vector3D())
+        self.assertEqual(w, w_2)
+
+        self.assertEqual(w.channel_mapping,  [[1, 2, 3], [4, 5, 6, 7, 8, 9]])
+        channel_mapping_ref = w.channel_mapping
+        channel_mapping_ref[0] = [10, 11]
+        self.assertEqual(w.channel_mapping, channel_mapping_ref)
+        self.assertNotEqual(w, w_2)
+        w.channel_mapping = [[1, 2, 3], [4, 5, 6, 7, 8, 9]]
+        self.assertEqual(w.channel_mapping,  [[1, 2, 3], [4, 5, 6, 7, 8, 9]])
+        self.assertEqual(w, w_2)
+
+        # Vector of weak pointers cannot be referenced, thus a copy is made
+        self.assertEqual(w.channel_excitations, [ex, ex_2])
+        channel_excitations_2 = w.channel_excitations
+        channel_excitations_2[0] = ex_3
+        self.assertNotEqual(w.channel_excitations, channel_excitations_2)
+        self.assertEqual(w, w_2)
+        w.channel_excitations = [ex_3, ex_4, ex_2]
+        self.assertEqual(w.channel_excitations, [ex_3, ex_4, ex_2])
         self.assertNotEqual(w, w_2)
         w_2 = urx.Wave(w)
-
-        w.parameters = [7.89, 9.87]
-        self.assertNotEqual(w, w_2)
-        w_2 = urx.Wave(w)
+        self.assertEqual(w, w_2)
 
         del ex_4
         gc.collect()
 
-        self.assertEqual(len(w.channel_excitations), 2)
-        self.assertEqual(len(w_2.channel_excitations), 2)
+        self.assertEqual(len(w.channel_excitations), 3)
+        self.assertEqual(len(w_2.channel_excitations), 3)
 
         self.assertIsNone(w.channel_excitations[1])
         self.assertIsNone(w_2.channel_excitations[1])
+
+        self.assertEqual(w.channel_delays, [3.14, 42])
+        channel_delays_ref = w.channel_delays
+        channel_delays_ref[0] = 123
+        self.assertEqual(w.channel_delays, channel_delays_ref)
+        self.assertNotEqual(w, w_2)
+        w.channel_delays = [3.14, 42]
+        self.assertEqual(w.channel_delays, [3.14, 42])
+        self.assertEqual(w, w_2)
+
+        self.assertEqual(w.parameters, [12.34, 56.78])
+        parameters_ref = w.parameters
+        parameters_ref[0] = 123
+        self.assertEqual(w.parameters, parameters_ref)
+        self.assertNotEqual(w, w_2)
+        w.parameters = [12.34, 56.78]
+        self.assertEqual(w.parameters, [12.34, 56.78])
+        self.assertEqual(w, w_2)
 
         print("--Test %s END--" % testName)
 
