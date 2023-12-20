598ef80814dc9d666af9657e0b31e1cacfdb0562
Thibault GEFFROY <thibault.geffroy@moduleus.com>
2023-12-20 10:31:48 +0100

    TransmitSetup

--- test/python/test_bindings_TransmitSetup.py
+++ test/python/test_bindings_TransmitSetup.py
@@ -10,6 +10,7 @@ class TestBindingsTransmitSetup(unittest.TestCase):
         testName = "TransmitSetup"
         print("\n--Test %s binding BEGIN--" % testName)
 
+        # Check default CTOR
         ts = urx.TransmitSetup()
         self.assertRaises(
             RuntimeError, lambda ts: ts.probe, ts)
@@ -18,13 +19,13 @@ class TestBindingsTransmitSetup(unittest.TestCase):
         self.assertEqual(ts.probe_transform, urx.Transform())
         self.assertEqual(ts.time_offset, urx.DoubleNan(0))
 
-        ts_2 = urx.TransmitSetup(ts)
-
         v = urx.Vector3D()
         v_2 = urx.Vector3D(1, 2, 3)
 
         t_2 = urx.Transform(v_2, v_2)
 
+        # Check copy CTOR and referencing object
+        ts_2 = urx.TransmitSetup(ts)
         self.assertEqual(ts, ts_2)
         ts_2.probe_transform = t_2
         self.assertNotEqual(ts, ts_2)
@@ -32,6 +33,7 @@ class TestBindingsTransmitSetup(unittest.TestCase):
         ts_ref.probe_transform = t_2
         self.assertEqual(ts, ts_ref)
 
+        # Check CTOR with all parameters
         ts = urx.TransmitSetup(urx.Probe(), urx.Wave(), t_2, 42)
         self.assertRaises(
             RuntimeError, lambda ts: ts.probe, ts)
@@ -56,11 +58,7 @@ class TestBindingsTransmitSetup(unittest.TestCase):
 
         ts = urx.TransmitSetup(probe, wave, t_2, 42)
 
-        self.assertEqual(ts.probe_transform, t_2)
-        t_2.rotation = v
-        self.assertEqual(ts.probe_transform, urx.Transform(v_2, v_2))
-        self.assertNotEqual(ts.probe_transform, t_2)
-
+        # probe is a pointer and thus shared between objects
         self.assertEqual(ts.probe, probe)
         ts.probe = probe_2
         self.assertEqual(ts.probe, probe_2)
@@ -71,6 +69,7 @@ class TestBindingsTransmitSetup(unittest.TestCase):
         self.assertRaises(
             RuntimeError, lambda ts: ts.probe, ts)
 
+        # wave is a pointer and thus shared between objects
         self.assertEqual(ts.wave, wave)
         ts.wave = wave_2
         self.assertEqual(ts.wave, wave_2)
@@ -81,13 +80,36 @@ class TestBindingsTransmitSetup(unittest.TestCase):
         self.assertRaises(
             RuntimeError, lambda ts: ts.wave, ts)
 
+        # Reference is possible for probe_transform
+        ts_2 = urx.TransmitSetup(ts)
+        self.assertEqual(ts.probe_transform, t_2)
+        t_2.rotation = v
+        self.assertEqual(ts, ts_2)
+        self.assertEqual(ts.probe_transform, urx.Transform(v_2, v_2))
+        self.assertNotEqual(ts.probe_transform, t_2)
         t_ref = ts.probe_transform
         t_ref.rotation = v
         self.assertEqual(ts.probe_transform, t_ref)
+        self.assertNotEqual(ts, ts_2)
+        # Check affectation
+        ts.probe_transform = urx.Transform(v_2, v_2)
+        self.assertEqual(ts.probe_transform, t_ref)
+        self.assertEqual(ts.probe_transform, urx.Transform(v_2, v_2))
+        self.assertEqual(ts, ts_2)
 
+        # Reference is possible for time_offset (DoubleNan)
+        self.assertEqual(ts.time_offset, 42)
         time_offset_ref = ts.time_offset
         time_offset_ref.value = 123
-        self.assertEqual(ts.time_offset, time_offset_ref)
+        self.assertEqual(ts.time_offset,
+                         time_offset_ref)
+        self.assertNotEqual(ts, ts_2)
+        # Check affectation
+        ts.time_offset = urx.DoubleNan(42)
+        self.assertEqual(ts.time_offset,
+                         time_offset_ref)
+        self.assertEqual(ts.time_offset, 42)
+        self.assertEqual(ts, ts_2)
 
         print("--Test %s END--" % testName)
 
