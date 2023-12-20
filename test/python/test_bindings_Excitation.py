c360fc4d4104d7c1bbd437f3d1e1c6d43bd54b8f
Thibault GEFFROY <thibault.geffroy@moduleus.com>
2023-12-20 09:49:25 +0100

    Excistation

--- test/python/test_bindings_Excitation.py
+++ test/python/test_bindings_Excitation.py
@@ -9,20 +9,29 @@ class TestBindingsExcitation(unittest.TestCase):
         testName = "Excitation binding"
         print("\n--Test %s BEGIN--" % testName)
 
+        # Check default CTOR
         ex = urx.Excitation()
-        ex_2 = urx.Excitation()
-
         self.assertEqual(ex.pulse_shape, "")
         self.assertTrue(np.isnan(ex.transmit_frequency.value))
         self.assertTrue(np.isnan(ex.sampling_frequency.value))
         self.assertEqual(len(ex.waveform), 0)
-        self.assertEqual(ex, ex_2)
 
+        # Check copy CTOR and referencing object
+        ex_2 = urx.Excitation(ex)
+        self.assertEqual(ex, ex_2)
+        ex_2.pulse_shape = "Hello"
+        self.assertNotEqual(ex, ex_2)
+        ex_ref = ex
+        ex_ref.pulse_shape = "Hello"
+        self.assertEqual(ex, ex_ref)
+
+        # Check CTOR with all parameters
         ex = urx.Excitation("linear", 42, np.nan, urx.VecFloat64([3.14, -42]))
         self.assertEqual(ex, urx.Excitation("linear", 42, np.nan, [3.14, -42]))
         ex_2 = urx.Excitation(ex)
         self.assertEqual(ex, ex_2)
 
+        # Check CTOR with all parameters varying double and DoubleNan
         ex = urx.Excitation("linear", urx.DoubleNan(42), np.nan, [3.14, -42])
         self.assertEqual(ex, ex_2)
         ex = urx.Excitation("linear", 42, urx.DoubleNan(np.nan), [3.14, -42])
@@ -31,52 +40,69 @@ class TestBindingsExcitation(unittest.TestCase):
             np.nan), urx.VecFloat64([3.14, -42]))
         self.assertEqual(ex, ex_2)
 
-        waveform = ex.waveform
-        self.assertEqual(waveform, [3.14, -42])
-        waveform[0] = 123.456
-        self.assertEqual(waveform, ex.waveform)
-        waveform.append(987)
-        self.assertEqual(waveform, ex.waveform)
+        # Reference is possible for urx.VecFloat64
+        self.assertEqual(ex, ex_2)
+        waveform_ref = ex.waveform
+        self.assertEqual(waveform_ref, [3.14, -42])
+        waveform_ref[0] = 123.456
+        self.assertEqual(waveform_ref, ex.waveform)
+        waveform_ref.append(987)
+        self.assertEqual(waveform_ref, ex.waveform)
         ex.waveform.append(852)
-        self.assertEqual(waveform, ex.waveform)
+        self.assertEqual(waveform_ref, ex.waveform)
+        self.assertNotEqual(ex, ex_2)
+        # Check affectation
+        ex.waveform = [3.14, -42]
+        self.assertEqual(waveform_ref, [3.14, -42])
 
+        # Reference is possible for DoubleNan
+        self.assertEqual(ex, ex_2)
         ex.transmit_frequency = urx.DoubleNan(np.nan)
-        transmit_frequency = ex.transmit_frequency
-        self.assertEqual(transmit_frequency, ex.transmit_frequency)
-        self.assertEqual(transmit_frequency, urx.DoubleNan(np.nan))
-        self.assertNotEqual(transmit_frequency.value, np.nan)
+        transmit_frequency_ref = ex.transmit_frequency
+        self.assertEqual(transmit_frequency_ref, ex.transmit_frequency)
+        self.assertEqual(transmit_frequency_ref, urx.DoubleNan(np.nan))
+        self.assertNotEqual(transmit_frequency_ref.value, np.nan)
+        # Check affectation
         ex.transmit_frequency = urx.DoubleNan(10)
-        self.assertEqual(transmit_frequency, ex.transmit_frequency)
-        transmit_frequency += 12
-        self.assertEqual(transmit_frequency, ex.transmit_frequency)
-        transmit_frequency -= 12
-        self.assertEqual(transmit_frequency, ex.transmit_frequency)
-        transmit_frequency *= 12
-        self.assertEqual(transmit_frequency, ex.transmit_frequency)
-        transmit_frequency /= 12
-        self.assertEqual(transmit_frequency, ex.transmit_frequency)
-        transmit_frequency = 123
-        self.assertNotEqual(transmit_frequency, ex.transmit_frequency)
+        self.assertEqual(transmit_frequency_ref, ex.transmit_frequency)
+        transmit_frequency_ref += 12
+        self.assertEqual(transmit_frequency_ref, ex.transmit_frequency)
+        transmit_frequency_ref -= 12
+        self.assertEqual(transmit_frequency_ref, ex.transmit_frequency)
+        transmit_frequency_ref *= 12
+        self.assertEqual(transmit_frequency_ref, ex.transmit_frequency)
+        transmit_frequency_ref /= 12
+        self.assertEqual(transmit_frequency_ref, ex.transmit_frequency)
+        transmit_frequency_ref = 123
+        self.assertNotEqual(transmit_frequency_ref, ex.transmit_frequency)
+        self.assertNotEqual(ex, ex_2)
+        ex.transmit_frequency = urx.DoubleNan(42)
 
+        # Reference is possible for DoubleNan
+        self.assertEqual(ex, ex_2)
         ex.sampling_frequency = urx.DoubleNan(np.nan)
-        sampling_frequency = ex.sampling_frequency
-        self.assertEqual(sampling_frequency, ex.sampling_frequency)
-        self.assertEqual(sampling_frequency, urx.DoubleNan(np.nan))
-        self.assertNotEqual(sampling_frequency.value, np.nan)
+        sampling_frequency_ref = ex.sampling_frequency
+        self.assertEqual(sampling_frequency_ref, ex.sampling_frequency)
+        self.assertEqual(sampling_frequency_ref, urx.DoubleNan(np.nan))
+        self.assertNotEqual(sampling_frequency_ref.value, np.nan)
+        # Check affectation
         ex.sampling_frequency.value = 10
-        self.assertEqual(sampling_frequency, ex.sampling_frequency)
-        sampling_frequency += 12
-        self.assertEqual(sampling_frequency, ex.sampling_frequency)
-        sampling_frequency -= 12
-        self.assertEqual(sampling_frequency, ex.sampling_frequency)
-        sampling_frequency *= 12
-        self.assertEqual(sampling_frequency, ex.sampling_frequency)
-        sampling_frequency /= 12
-        self.assertEqual(sampling_frequency, ex.sampling_frequency)
-        sampling_frequency = urx.DoubleNan(123)
-        self.assertNotEqual(sampling_frequency, ex.sampling_frequency)
+        self.assertEqual(sampling_frequency_ref, ex.sampling_frequency)
+        sampling_frequency_ref += 12
+        self.assertEqual(sampling_frequency_ref, ex.sampling_frequency)
+        sampling_frequency_ref -= 12
+        self.assertEqual(sampling_frequency_ref, ex.sampling_frequency)
+        sampling_frequency_ref *= 12
+        self.assertEqual(sampling_frequency_ref, ex.sampling_frequency)
+        sampling_frequency_ref /= 12
+        self.assertEqual(sampling_frequency_ref, ex.sampling_frequency)
+        sampling_frequency_ref = urx.DoubleNan(123)
+        self.assertNotEqual(sampling_frequency_ref, ex.sampling_frequency)
+        self.assertNotEqual(ex, ex_2)
+        ex.sampling_frequency = urx.DoubleNan()
 
-        # No ref possible for string
+        # Reference is not possible for string
+        self.assertEqual(ex, ex_2)
         pulse_shape = ex.pulse_shape
         self.assertEqual(pulse_shape, ex.pulse_shape)
         ex.pulse_shape = "Hello"
