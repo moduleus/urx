14b339d34f71ea21a2fa920831e24b913e8d269b
Thibault GEFFROY <thibault.geffroy@moduleus.com>
2024-01-08 12:10:28 +0100

    Unlink soft and file version

---
+++ urx/urx.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <cstdint>
+
+namespace urx {
+
+constexpr const uint16_t URX_VERSION_MAJOR(1);
+constexpr const uint16_t URX_VERSION_MINOR(0);
+constexpr const uint16_t URX_VERSION_PATCH(0);
+
+}  // namespace urx
