9d0428ba7247acc9ad7e5a8373eaa91af468d269
Vincent LE GARREC <vincent.legarrec@moduleus.com>
2024-08-23 09:47:11 +0200

    pip wheel from CMakeLists.txt

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setuptools.setup(
         )
     ],
     cmdclass=dict(build_ext=cmake_build_extension.BuildExtension),
-    version="0.0.1",
+    version="0.6.26",
     packages=["pyurx"],
     package_dir={
         "pyurx": "python/pyurx",
