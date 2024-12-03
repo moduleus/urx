93c0f36d29c4ba6b46750115bda95a7ca0cc7250
Thibault GEFFROY <thibault.geffroy@moduleus.com>
2024-12-03 10:05:39 +0100

    Add new function getEltId and associated test

--- urx/utils/common.h
+++ urx/utils/common.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include <vector>
 #include <version>
 
 #include <urx/utils/export.h>
@@ -17,5 +18,17 @@ namespace urx::utils {
 ///\return The index value as a string with as many necessary leading zeros to fill length characters
 ///
 URX_UTILS_EXPORT std::string formatIndexWithLeadingZeros(const std::size_t index, const int length);
+
+/// Return the first index corresponding to the target element in the element vector
+/// If the element does not belong to the vector then -1 is returned
+template <typename T>
+int32_t getEltId(const std::vector<T> &elt_vec, const T &elt) {
+  const ptrdiff_t res_id =
+      std::distance(elt_vec.cbegin(), std::find(elt_vec.cbegin(), elt_vec.cend(), elt));
+  if (res_id < 0 || static_cast<size_t>(res_id) >= elt_vec.size()) {
+    return -1;
+  }
+  return res_id;
+};
 
 }  // namespace urx::utils
