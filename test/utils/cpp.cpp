30ec9676b341daa5cbdcfc8c84300544ae486660
Thibault GEFFROY <thibault.geffroy@moduleus.com>
2024-12-04 16:05:49 +0100

    Move cpp utils to dedicated file

--- test/utils/cpp.cpp
+++ test/utils/cpp.cpp
@@ -1,5 +1,6 @@
 #include <memory>
 #include <optional>
+#include <vector>
 
 #include <catch2/catch_test_macros.hpp>
 
@@ -29,6 +30,19 @@ TEST_CASE("is_optional", "[Cpp]") {
   REQUIRE(!urx::utils::is_optional<std::shared_ptr<double>>::value);
   REQUIRE(!urx::utils::is_optional<std::weak_ptr<double>>::value);
   REQUIRE(urx::utils::is_optional<std::optional<double>>::value);
+}
+
+TEST_CASE("TypeContainer", "[Cpp]") {
+  REQUIRE(urx::utils::TypeContainer<double>::VALUE == urx::utils::ContainerType::RAW);
+  REQUIRE(urx::utils::TypeContainer<double*>::VALUE == urx::utils::ContainerType::RAW);
+  REQUIRE(urx::utils::TypeContainer<std::vector<double>>::VALUE ==
+          urx::utils::ContainerType::VECTOR);
+  REQUIRE(urx::utils::TypeContainer<std::optional<double>>::VALUE ==
+          urx::utils::ContainerType::OPTIONAL);
+  REQUIRE(urx::utils::TypeContainer<std::shared_ptr<double>>::VALUE ==
+          urx::utils::ContainerType::SHARED_PTR);
+  REQUIRE(urx::utils::TypeContainer<std::weak_ptr<double>>::VALUE ==
+          urx::utils::ContainerType::WEAK_PTR);
 }
 
 }  // namespace urx::utils::test
