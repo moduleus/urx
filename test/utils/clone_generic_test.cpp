5999d2491ca0b17f9f239270c331645f1a10eab6
Thibault GEFFROY <thibault.geffroy@moduleus.com>
2024-12-05 09:05:07 +0100

    Clone: Refactor code

---
+++ test/utils/clone_generic_test.cpp
@@ -0,0 +1,46 @@
+#include <memory>
+
+#include <catch2/catch_test_macros.hpp>
+#include <test/utils/clone_generic_test.h>
+
+#include <urx/acquisition.h>
+#include <urx/dataset.h>
+#include <urx/detail/double_nan.h>
+#include <urx/element.h>
+#include <urx/element_geometry.h>
+#include <urx/event.h>
+#include <urx/excitation.h>
+#include <urx/group.h>
+#include <urx/group_data.h>
+#include <urx/impulse_response.h>
+#include <urx/probe.h>
+#include <urx/receive_setup.h>
+#include <urx/transform.h>
+#include <urx/transmit_setup.h>
+#include <urx/vector.h>
+#include <urx/version.h>
+#include <urx/wave.h>
+
+namespace urx::utils::test {
+
+TEST_CASE("Clone Vector2D", "[Clone]") {
+  Vector2D<double> d_vec = {1.23, 4.56};
+  generic_clone_test(d_vec);
+}
+
+TEST_CASE("Clone Vector3D", "[Clone]") {
+  Vector3D<double> d_vec = {1.23, 4.56, -7.89};
+  generic_clone_test(d_vec);
+}
+
+TEST_CASE("Clone Version", "[Clone]") {
+  Version vers = {1, 2, 3};
+  generic_clone_test(vers);
+}
+
+TEST_CASE("Clone Transform", "[Clone]") {
+  Transform t = {{1, 2, 3}, {4, 5, 6}};
+  generic_clone_test(t);
+}
+
+}  // namespace urx::utils::test
